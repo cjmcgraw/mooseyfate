dc43706dec808b823b483ce03a69fcccd0417375
Carl McGraw <cjmcgraw@uw.edu>
2015-07-19 14:30:46 -0700

    Updated gitignore to track lib directory

---
+++ lib/HelperFunctions.py
@@ -0,0 +1,8 @@
+"""This module defines some helper functions that are useful
+to exist in a single place
+"""
+from math import sqrt
+from random import random, randint
+
+def euclidean_distance(initial_vector, to_vector):
+    return sqrt(sum([(x2 - x1) ** 2 for x1, x2 in zip(initial_vector, to_vector)]))
